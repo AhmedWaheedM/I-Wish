14bde3b65561c4c9d7da5efcf490489116fb8c0d
asemEmad47 <90583040+asemEmad47@users.noreply.github.com>
2026-01-19 17:26:31 +0200

    Adding new application mains conf

--- iwish/src/main/java/module-info.java
+++ iwish/src/main/java/module-info.java
@@ -2,6 +2,9 @@ module edu.iti.javaii.project {
     requires javafx.controls;
     requires javafx.fxml;
     requires java.sql;
-    opens edu.iti.javaii.project to javafx.fxml;
-    exports edu.iti.javaii.project;
+
+    exports clientSide;
+    opens clientSide;
+    opens clientSide.appManger;
+    opens clientSide.controllers;
 }
