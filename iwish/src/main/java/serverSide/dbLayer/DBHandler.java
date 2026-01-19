2976f27f9e49c8b11227bd9d176f20a544c177c9
asemEmad47 <90583040+asemEmad47@users.noreply.github.com>
2026-01-19 17:28:57 +0200

    Making DB handler reading Db conf from db.properties in resources

--- iwish/src/main/java/serverSide/dbLayer/DBHandler.java
+++ iwish/src/main/java/serverSide/dbLayer/DBHandler.java
@@ -1,21 +1,47 @@
 package serverSide.dbLayer;
 
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Properties;
 
 public abstract class DBHandler {
-    protected final String dbUrl = "jdbc:mysql://localhost:3306/your_db";
-    protected final String dbName = "your_db";
-    protected final String username = "root";
-    protected final String password = "password";
+    protected static String dbUrl ;
+    protected static String dbName ;
+    protected static String username ;
+    protected static String password ;
     protected String tableName;
 
     protected Connection connection;
     protected Statement statement;
     protected ResultSet resultSet;
+
+    static {
+        try (InputStream input =
+                    DBHandler.class
+                            .getClassLoader()
+                            .getResourceAsStream("db.properties")
+            ) {
+
+            if (input == null) {
+                throw new RuntimeException("db.properties not found in resources");
+            }
+
+            Properties props = new Properties();
+            props.load(input);
+
+            dbUrl = props.getProperty("db.url");
+            dbName = props.getProperty("db.name");
+            username = props.getProperty("db.username");
+            password = props.getProperty("db.password");
+
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to load DB configuration", e);
+        }
+    }
 
     public DBHandler(String tableName) {
         this.tableName = tableName;
@@ -23,7 +49,13 @@ public abstract class DBHandler {
 
     protected void connect() throws SQLException {
         if (connection == null || connection.isClosed()) {
-            connection = DriverManager.getConnection(dbUrl, username, password);
+
+            try {
+                connection = DriverManager.getConnection(dbUrl, username, password);
+                System.err.println("Connection established ");
+            } catch (Exception e) {
+                System.err.println("error: " + e.getMessage());
+            }
         }
     }
 
